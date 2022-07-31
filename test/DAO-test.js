3f6819a271cb457f09b4b64495a70df205e371d5
kulho <jakubkulhanek82103@gmail.com>
2022-07-31 23:29:18 +0200

    starting to work on DAO test

--- test/DAO-test.js
+++ test/DAO-test.js
@@ -5,7 +5,7 @@ const { time } = require("@openzeppelin/test-helpers");
 const { assertion } = require("@openzeppelin/test-helpers/src/expectRevert");
 
 // Proposal
-PROPOSAL_DESCRIPTION = "Proposal #1: Store 5 in the Box!";
+PROPOSAL_DESCRIPTION = "Proposal #1: Integration test";
 NEW_STORE_VALUE = 5;
 VOTING_DELAY = 10; /* 10 block */
 // VOTING_PERIOD = 45818; /* 1 week */
@@ -13,10 +13,15 @@ VOTING_PERIOD = 20; /* testing purposes */
 VOTE = 1; // 0 = Against, 1 = For, 2 = Abstain for this example
 MIN_DELAY = 3600; // one hour
 
-contract("DAO tests", (accounts) => {
-  let box;
-  let governorContract;
-  let governanceTimeLock;
+contract("DAO integration test", (accounts) => {
+  let governorContract,
+    governanceTimeLock,
+    token,
+    nft,
+    stakingPool,
+    uniswapPool,
+    treasury;
+
   beforeEach(async () => {
     governorContract = await GovernorContract.deployed();
     governanceTimeLock = await GovernanceTimeLock.deployed();
@@ -25,7 +30,18 @@ contract("DAO tests", (accounts) => {
     await time.advanceBlockTo(deployBlock.number + VOTING_DELAY);
   });
 
-  it("create and pass a proposal", async () => {
+  it("emulate actual usage of the protocol", async () => {
+    /*
+    Test scenario:
+    - claim some nfts
+    - get tokens for nfts
+    - stake token in staking pool
+    - make trades in uniswap pool to generate fees
+    - send some ether to the treasy to emulate gain of royalties
+    - create and pass proposal to collect fees, then swap everything
+      for token and approve to pool, notify rewards to the staking pool
+    - make time pass and collect rewards from the staking pool
+    */
     encodedFunction = await box.contract.methods
       .store(NEW_STORE_VALUE)
       .encodeABI();
